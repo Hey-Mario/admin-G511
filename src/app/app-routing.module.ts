d6934b6516907e682feef77086daf8e6096e63cd
Mario Lovamanitra <lovamanitra.randriamanantena@esti.mg>
2022-10-17 17:57:22 +0300

    implementation de lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './shared/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'**', loadChildren:()=>import('./shared/shared.module').then(module => module.SharedModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
