ef8324e639f3938c7b7aee1bf1cbf913ec2400c8
Mario Lovamanitra <lovamanitra.randriamanantena@esti.mg>
2022-10-18 08:22:20 +0300

    OK avec le header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule } from '@angular/common/http'
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule
   ],
   exports:[
-    HeaderComponent,
     FooterComponent,
+    HeaderComponent
   ]
 })
 export class CoreModule { }
